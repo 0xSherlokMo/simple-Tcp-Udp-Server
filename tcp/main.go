253bb37fc5704ef0b7e7e3993863cb586175f4cb
0xSherlokMo <mohamed.sorour@halan.com>
2022-10-07 20:49:00 +0200

    add process worker to handle multiple requests at the same time

--- tcp/main.go
+++ tcp/main.go
@@ -1,11 +1,32 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"log"
 	"net"
 	"os"
 )
+
+func pcrocessWorker(connections <-chan net.Conn, id int) {
+	log.Printf("worker %d => up and ready", id)
+	for conn := range connections {
+		log.Printf("worker %d => Someone just connected to the magical server!", id)
+		buf := make([]byte, 1024)
+		for {
+			n, err := conn.Read(buf)
+			if err != nil {
+				if err == io.EOF {
+					log.Printf("worker %d => Someone just disconnected", id)
+				}
+				break
+			}
+
+			log.Printf("worker %d => received: %q", id, buf[:n])
+		}
+		conn.Close()
+	}
+}
 
 func main() {
 	lis, err := net.Listen("tcp", "127.0.0.1:"+os.Getenv("PORT"))
@@ -13,26 +34,18 @@ func main() {
 		log.Fatal(err)
 	}
 	log.Printf("Server is up and running on %s", lis.Addr())
+
+	connectionsChan := make(chan net.Conn, 10)
+	for i := 1; i <= 10; i++ {
+		go pcrocessWorker(connectionsChan, i)
+	}
+
 	for {
-		accept, err := lis.Accept()
+		connection, err := lis.Accept()
 		if err != nil {
 			log.Fatal(err)
 		}
-		go func(c net.Conn) {
-			defer c.Close()
-			log.Println("Someone just connected to the magical server!")
-			buf := make([]byte, 1024)
-			for {
-				n, err := c.Read(buf)
-				if err != nil {
-					if err == io.EOF {
-						log.Println("Someone just disconnected")
-					}
-					return
-				}
-
-				log.Printf("received: %q", buf[:n])
-			}
-		}(accept)
+		connectionsChan <- connection
+		fmt.Println("test")
 	}
 }
